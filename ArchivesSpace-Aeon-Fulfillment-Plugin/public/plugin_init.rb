be8db9dc1079857b4bda32e74b7521c004316d92
Austin Schaffer <aschaffer@atlas-sys.com>
2019-01-02 12:43:55 -0500

    Removed a submodule

--- ArchivesSpace-Aeon-Fulfillment-Plugin/public/plugin_init.rb
+++
@@ -1,9 +0,0 @@
-## Register our custom page action
-record_types = AppConfig.has_key?(:aeon_fulfillment_record_types) ? AppConfig[:aeon_fulfillment_record_types]
-                                                                  : ['archival_object', 'accession']
-
-button_position = AppConfig.has_key?(:aeon_fulfillment_button_position) ? AppConfig[:aeon_fulfillment_button_position] : nil
-
-Plugins::add_record_page_action_erb(record_types,
-                                    'aeon/aeon_request_action',
-                                    button_position)
