b3e0d54d10d30142326fa86cb49562cc91a9d578
Austin Schaffer <aschaffer@atlas-sys.com>
2019-01-04 09:09:37 -0500

    Changes to test config

--- docker/config.rb
+++ docker/config.rb
@@ -3,15 +3,10 @@ AppConfig[:plugins] = ['aeon_fulfillment']
 
 AppConfig[:aeon_fulfillment] = {
     "test" => {
-        :aeon_web_url => "https://your.institution.edu/aeon/aeon.dll",        
+        :aeon_web_url => "https://your.institution.edu/aeon/aeon.dll",
         :aeon_return_link_label => "Back to ArchivesSpace",
         :aeon_external_system_id => "ArchivesSpace",
-
-        ## Options: 'whitelist' or 'blacklist'
-        :requests_permitted_for_archival_record_levels => {
-            :list_type => 'whitelist',
-            :values => ['file', 'item']
-        }
+        :requestable_archival_record_levels => ['file', 'item', 'otherlevel']
     }
 }
 
