c1f09c43acf5b05a9114859635492686e22102c2
Austin Schaffer <aschaffer@atlas-sys.com>
2019-01-15 11:59:31 -0500

    updated config

--- docker/config.rb
+++ docker/config.rb
@@ -6,7 +6,11 @@ AppConfig[:aeon_fulfillment] = {
         :aeon_web_url => "https://your.institution.edu/aeon/aeon.dll",
         :aeon_return_link_label => "Back to ArchivesSpace",
         :aeon_external_system_id => "ArchivesSpace",
-        :requestable_archival_record_levels => ['file', 'item', 'otherlevel']
+        :requestable_archival_record_levels => ['file', 'item', 'otherlevel'],
+        :requests_permitted_for_containers_only => true,
+        :user_defined_fields => {
+            :list_type => :blacklist,
+            :values => ["string_1", "text_2", "boolean_1", "boolean_3"]
+        }
     }
 }
-
